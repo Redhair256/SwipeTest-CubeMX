bea8adbfb59b61eb67d9f32924573c60fff055eb
Sergey <redhair20@hotmail.com>
2019-09-06 12:58:28 +0300

    Configure Port A+B+C LCD Output

--- Inc/main.h
+++ Inc/main.h
@@ -30,6 +30,7 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l1xx_hal.h"
 #include "stm32l1xx_it.h"
+//#include "stm32l152xC.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -78,7 +79,13 @@ void UpdateDisplay(void);
 #define SEG17_Pin GPIO_PIN_3
 #define SEG17_GPIO_Port GPIOC
 #define PA0_Pin GPIO_PIN_0
+
 #define PA0_GPIO_Port GPIOA
+#define PB0_GPIO_Port GPIOB
+#define PC0_GPIO_Port GPIOC
+
+#define AHBENR_Port 0x1C
+
 #define PA0_EXTI_IRQn EXTI0_IRQn
 #define SEG0_Pin GPIO_PIN_1
 #define SEG0_GPIO_Port GPIOA
@@ -151,6 +158,24 @@ void UpdateDisplay(void);
 #define COM3_Pin GPIO_PIN_9
 #define COM3_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
+
+#define GPIO_Pin_0                 ((uint16_t)0x0001)  /*!< Pin 0 selected */
+#define GPIO_Pin_1                 ((uint16_t)0x0002)  /*!< Pin 1 selected */
+#define GPIO_Pin_2                 ((uint16_t)0x0004)  /*!< Pin 2 selected */
+#define GPIO_Pin_3                 ((uint16_t)0x0008)  /*!< Pin 3 selected */
+#define GPIO_Pin_4                 ((uint16_t)0x0010)  /*!< Pin 4 selected */
+#define GPIO_Pin_5                 ((uint16_t)0x0020)  /*!< Pin 5 selected */
+#define GPIO_Pin_6                 ((uint16_t)0x0040)  /*!< Pin 6 selected */
+#define GPIO_Pin_7                 ((uint16_t)0x0080)  /*!< Pin 7 selected */
+#define GPIO_Pin_8                 ((uint16_t)0x0100)  /*!< Pin 8 selected */
+#define GPIO_Pin_9                 ((uint16_t)0x0200)  /*!< Pin 9 selected */
+#define GPIO_Pin_10                ((uint16_t)0x0400)  /*!< Pin 10 selected */
+#define GPIO_Pin_11                ((uint16_t)0x0800)  /*!< Pin 11 selected */
+#define GPIO_Pin_12                ((uint16_t)0x1000)  /*!< Pin 12 selected */
+#define GPIO_Pin_13                ((uint16_t)0x2000)  /*!< Pin 13 selected */
+#define GPIO_Pin_14                ((uint16_t)0x4000)  /*!< Pin 14 selected */
+#define GPIO_Pin_15                ((uint16_t)0x8000)  /*!< Pin 15 selected */
+#define GPIO_Pin_All               ((uint16_t)0xFFFF)  /*!< All pins selected */
 
 /* USER CODE END Private defines */
 
